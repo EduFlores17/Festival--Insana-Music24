425fbbafd86f0607fb9bf52624d87e12f8892557
Temovlog17 <fedu17t@gmail.com>
2024-07-29 14:39:50 -0600

    C3.5 - JS y mas contenido

---
+++ build/js/app.js
@@ -0,0 +1,107 @@
+document.addEventListener('DOMContentLoaded', function(){
+    navegacionFija()
+    crearGaleria()
+    resaltarEnlace()
+    scrollNav()
+})
+
+function navegacionFija(){
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
+    
+    window.addEventListener('scroll', function(){
+        if(sobreFestival.getBoundingClientRect().bottom <1){
+            header.classList.add('fixed')
+        }else{
+            header.classList.remove('fixed')
+        }
+    })
+}
+
+function crearGaleria(){
+    const galeria = document.querySelector('.galeria-imagenes')
+    const cantidad_imagenes= 16
+
+
+    for(let i =1; i<=cantidad_imagenes; i++){
+        const imagen = document.createElement('IMG')
+        imagen.src= `src/img/${i}.jpg`
+        imagen.alt= 'imagen galeria'
+        //event hander
+        imagen.onclick = function(){
+            mostrarImagen(i)
+        }
+        galeria.appendChild(imagen)
+    }
+}
+
+function mostrarImagen(i){
+    const imagen = document.createElement('IMG')
+    imagen.src= `src/img/${i}.jpg`
+    imagen.alt= 'imagen galeria'
+    //generar modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+    
+    //boton con cierre
+    const cerrarModalBtn= document.createElement('BUTTON') 
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+    //agregamos al html
+    const body = document.querySelector('body')
+    body.appendChild(modal)
+    body.classList.add('overflow-hidden')
+}
+
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+    setTimeout(() => {
+        modal?.remove()
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 600);
+}
+
+function resaltarEnlace(){
+    document.addEventListener('scroll', function (){
+        const section = document.querySelectorAll('section')
+        const navLinks= document.querySelectorAll('.navegacion-principal a')
+
+        let actual = '';
+
+        section.forEach(section =>{
+            const sectionTop = section.offsetTop
+            const sectionHeight = section.clientHeight
+
+            if(window.scrollY >= (sectionTop - sectionHeight/3)){
+                actual = section.id
+            }
+        })
+        navLinks.forEach(link =>{
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav(){
+    const navLinks= document.querySelectorAll('.navegacion-principal a')
+    navLinks.forEach(link =>{
+        link.addEventListener('click', e =>{
+            e.preventDefault()
+            const sectionScroll = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior: 'smooth'})
+        })
+    })
+}
